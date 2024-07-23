93c6f038c82c9e1ea134e0ca6b907ddeb015ab7c
Sidharth Menon <sidharth@startupmission.in>
2024-07-23 16:46:13 +0000

    initial

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+		content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    safelist: [
+        {
+          pattern: /max-w-\dxl/,
+          variants: ['sm', 'md', 'lg', 'xl']
+        }
+    ],    
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: colors.blue,
+                success: colors.green,
+                warning: colors.yellow,
+                dark: "#003B89",
+                light: "#7CBDD0"
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
 }
